b15b6b1f175d5f432a6332cf6bba943e8d0a9b0d
Jasonnor <wujason810@gmail.com>
2018-08-16 13:57:37 +0800

    Add custom mask

--- libs/util.py
+++ libs/util.py
@@ -9,6 +9,32 @@ def random_mask(height, width, channels=3):
     img = np.zeros((height, width, channels), np.uint8)
 
     # Set size scale
+    size = int(100)
+    if width < 64 or height < 64:
+        raise Exception("Width and Height of mask must be at least 64!")
+        
+    # Draw random circles
+    if randint(0, 1) == 1:
+        for _ in range(1):
+            x1, y1 = randint(1, width), randint(1, height)
+            radius = randint(20, size)
+            cv2.circle(img,(x1,y1),radius,(1,1,1), -1)
+    else:
+    # Draw random ellipses
+        for _ in range(1):
+            x1, y1 = randint(1, width), randint(1, height)
+            s1, s2 = randint(15, size), randint(15, size)
+            a1, a2, a3 = randint(0, 180), 0, 360
+            thickness = randint(3, 30)
+            cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,(1,1,1), -1)
+    
+    return 1-img
+
+def random_mask_ori(height, width, channels=3):
+    """Generates a random irregular mask with lines, circles and elipses"""    
+    img = np.zeros((height, width, channels), np.uint8)
+
+    # Set size scale
     size = int((width + height) * 0.03)
     if width < 64 or height < 64:
         raise Exception("Width and Height of mask must be at least 64!")
