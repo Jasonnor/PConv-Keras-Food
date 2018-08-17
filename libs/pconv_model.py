89e1781c12f6f7e036cbbe9eec7d0c5656169057
Jasonnor <wujason810@gmail.com>
2018-08-17 11:53:19 +0800

    Formate code

--- libs/pconv_model.py
+++ libs/pconv_model.py
@@ -1,10 +1,8 @@
 import os
 from datetime import datetime
-
 from keras.models import Model
-from keras.models import load_model
 from keras.optimizers import Adam
-from keras.layers import Input, Conv2D, UpSampling2D, Dropout, LeakyReLU, BatchNormalization, Activation
+from keras.layers import Input, Conv2D, UpSampling2D, LeakyReLU, BatchNormalization, Activation
 from keras.layers.merge import Concatenate
 from keras.applications import VGG16
 from keras.applications import Xception
@@ -17,7 +15,7 @@ class PConvUnet(object):
 
     def __init__(self, img_rows=256, img_cols=256, weight_filepath=None):
         """Create the PConvUnet. If variable image size, set img_rows and img_cols to None"""
-        
+
         # Settings
         self.weight_filepath = weight_filepath
         self.img_rows = img_rows
@@ -27,15 +25,15 @@ class PConvUnet(object):
 
         # Set current epoch
         self.current_epoch = 0
-        
+
         # VGG layers to extract features from (first maxpooling layers, see pp. 7 of paper)
         self.vgg_layers = [3, 6, 10]
         self.xception_layers = [14, 24, 34]
-        
+
         # Get the vgg16 model for perceptual loss
-        #self.vgg_origin = self.build_vgg()
+        # self.vgg_origin = self.build_vgg()
         self.vgg = self.build_xception()
-        
+
         # Create UNet-like model
         self.model, self.parallel_model = self.build_pconv_unet()
 
@@ -58,7 +56,7 @@ class PConvUnet(object):
         model.compile(loss='mse', optimizer='adam')
 
         return model
-        
+
     def build_vgg(self):
         """
         Load pre-trained VGG16 from keras applications
@@ -78,25 +76,26 @@ class PConvUnet(object):
         model = Model(inputs=img, outputs=vgg(img))
         model.trainable = False
         model.compile(loss='mse', optimizer='adam')
-        
+
         return model
-        
-    def build_pconv_unet(self, train_bn=True, lr=0.0002):      
+
+    def build_pconv_unet(self, train_bn=True, lr=0.0002):
 
         # INPUTS
         inputs_img = Input((self.img_rows, self.img_cols, 3))
         inputs_mask = Input((self.img_rows, self.img_cols, 3))
-        
+
         # ENCODER
         def encoder_layer(img_in, mask_in, filters, kernel_size, bn=True):
             conv, mask = PConv2D(filters, kernel_size, strides=2, padding='same')([img_in, mask_in])
             if bn:
-                conv = BatchNormalization(name='EncBN'+str(encoder_layer.counter))(conv, training=train_bn)
+                conv = BatchNormalization(name='EncBN' + str(encoder_layer.counter))(conv, training=train_bn)
             conv = Activation('relu')(conv)
             encoder_layer.counter += 1
             return conv, mask
+
         encoder_layer.counter = 0
-        
+
         e_conv1, e_mask1 = encoder_layer(inputs_img, inputs_mask, 64, 7, bn=False)
         e_conv2, e_mask2 = encoder_layer(e_conv1, e_mask1, 128, 5)
         e_conv3, e_mask3 = encoder_layer(e_conv2, e_mask2, 256, 5)
@@ -105,19 +104,19 @@ class PConvUnet(object):
         e_conv6, e_mask6 = encoder_layer(e_conv5, e_mask5, 512, 3)
         e_conv7, e_mask7 = encoder_layer(e_conv6, e_mask6, 512, 3)
         e_conv8, e_mask8 = encoder_layer(e_conv7, e_mask7, 512, 3)
-        
+
         # DECODER
         def decoder_layer(img_in, mask_in, e_conv, e_mask, filters, kernel_size, bn=True):
-            up_img = UpSampling2D(size=(2,2))(img_in)
-            up_mask = UpSampling2D(size=(2,2))(mask_in)
-            concat_img = Concatenate(axis=3)([e_conv,up_img])
-            concat_mask = Concatenate(axis=3)([e_mask,up_mask])
+            up_img = UpSampling2D(size=(2, 2))(img_in)
+            up_mask = UpSampling2D(size=(2, 2))(mask_in)
+            concat_img = Concatenate(axis=3)([e_conv, up_img])
+            concat_mask = Concatenate(axis=3)([e_mask, up_mask])
             conv, mask = PConv2D(filters, kernel_size, padding='same')([concat_img, concat_mask])
             if bn:
                 conv = BatchNormalization()(conv)
             conv = LeakyReLU(alpha=0.2)(conv)
             return conv, mask
-            
+
         d_conv9, d_mask9 = decoder_layer(e_conv8, e_mask8, e_conv7, e_mask7, 512, 3)
         d_conv10, d_mask10 = decoder_layer(d_conv9, d_mask9, e_conv6, e_mask6, 512, 3)
         d_conv11, d_mask11 = decoder_layer(d_conv10, d_mask10, e_conv5, e_mask5, 512, 3)
@@ -126,41 +125,41 @@ class PConvUnet(object):
         d_conv14, d_mask14 = decoder_layer(d_conv13, d_mask13, e_conv2, e_mask2, 128, 3)
         d_conv15, d_mask15 = decoder_layer(d_conv14, d_mask14, e_conv1, e_mask1, 64, 3)
         d_conv16, d_mask16 = decoder_layer(d_conv15, d_mask15, inputs_img, inputs_mask, 3, 3, bn=False)
-        outputs = Conv2D(3, 1, activation = 'sigmoid')(d_conv16)        
-        
+        outputs = Conv2D(3, 1, activation='sigmoid')(d_conv16)
+
         # Setup the model inputs / outputs
         model = Model(inputs=[inputs_img, inputs_mask], outputs=outputs)
         parallel_model = multi_gpu_model(model, gpus=2)
-        
-        # Compile the model
+
+        # Compile the parallel model
         parallel_model.compile(
-            optimizer = Adam(lr=lr),
+            optimizer=Adam(lr=lr),
             loss=self.loss_total(inputs_mask)
         )
 
         # Compile the model
-        #model.compile(
+        # model.compile(
         #    optimizer = Adam(lr=lr),
         #    loss=self.loss_total(inputs_mask)
-        #)
+        # )
 
         return model, parallel_model
-    
+
     def loss_total(self, mask):
         """
         Creates a loss function which sums all the loss components 
         and multiplies by their weights. See paper eq. 7.
         """
+
         def loss(y_true, y_pred):
-            
             # Compute predicted image with non-hole pixels set to ground truth
-            y_comp = mask * y_true + (1-mask) * y_pred
-            
+            y_comp = mask * y_true + (1 - mask) * y_pred
+
             # Compute the vgg features
             vgg_out = self.vgg(y_pred)
             vgg_gt = self.vgg(y_true)
             vgg_comp = self.vgg(y_comp)
-            
+
             # Compute loss components
             l1 = self.loss_valid(mask, y_true, y_pred)
             l2 = self.loss_hole(mask, y_true, y_pred)
@@ -168,49 +167,49 @@ class PConvUnet(object):
             l4 = self.loss_style(vgg_out, vgg_gt)
             l5 = self.loss_style(vgg_comp, vgg_gt)
             l6 = self.loss_tv(mask, y_comp)
-            
+
             # Return loss function
-            return l1 + 6*l2 + 0.05*l3 + 120*(l4+l5) + 0.1*l6
+            return l1 + 6 * l2 + 0.05 * l3 + 120 * (l4 + l5) + 0.1 * l6
 
         return loss
-    
+
     def loss_hole(self, mask, y_true, y_pred):
         """Pixel L1 loss within the hole / mask"""
-        return self.l1((1-mask) * y_true, (1-mask) * y_pred)
-    
+        return self.l1((1 - mask) * y_true, (1 - mask) * y_pred)
+
     def loss_valid(self, mask, y_true, y_pred):
         """Pixel L1 loss outside the hole / mask"""
         return self.l1(mask * y_true, mask * y_pred)
-    
-    def loss_perceptual(self, vgg_out, vgg_gt, vgg_comp): 
-        """Perceptual loss based on VGG16, see. eq. 3 in paper"""       
+
+    def loss_perceptual(self, vgg_out, vgg_gt, vgg_comp):
+        """Perceptual loss based on VGG16, see. eq. 3 in paper"""
         loss = 0
         for o, c, g in zip(vgg_out, vgg_comp, vgg_gt):
             loss += self.l1(o, g) + self.l1(c, g)
         return loss
-        
+
     def loss_style(self, output, vgg_gt):
         """Style loss based on output/computation, used for both eq. 4 & 5 in paper"""
         loss = 0
         for o, g in zip(output, vgg_gt):
             loss += self.l1(self.gram_matrix(o), self.gram_matrix(g))
         return loss
-    
+
     def loss_tv(self, mask, y_comp):
         """Total variation loss, used for smoothing the hole region, see. eq. 6"""
 
         # Create dilated hole region using a 3x3 kernel of all 1s.
         kernel = K.ones(shape=(3, 3, mask.shape[3], mask.shape[3]))
-        dilated_mask = K.conv2d(1-mask, kernel, data_format='channels_last', padding='same')
+        dilated_mask = K.conv2d(1 - mask, kernel, data_format='channels_last', padding='same')
 
         # Cast values to be [0., 1.], and compute dilated hole region of y_comp
         dilated_mask = K.cast(K.greater(dilated_mask, 0), 'float32')
         P = dilated_mask * y_comp
 
         # Calculate total variation loss
-        a = self.l1(P[:,1:,:,:], P[:,:-1,:,:])
-        b = self.l1(P[:,:,1:,:], P[:,:,:-1,:])        
-        return a+b
+        a = self.l1(P[:, 1:, :, :], P[:, :-1, :, :])
+        b = self.l1(P[:, :, 1:, :], P[:, :, :-1, :])
+        return a + b
 
     def fit(self, generator, epochs=10, plot_callback=None, *args, **kwargs):
         """Fit the U-Net to a (images, targets) generator
@@ -219,21 +218,21 @@ class PConvUnet(object):
         param epochs: number of epochs to train for
         param plot_callback: callback function taking Unet model as parameter
         """
-        
+
         # Loop over epochs
-        for _ in range(epochs):            
-            
+        for _ in range(epochs):
+
             # Fit the model
             self.parallel_model.fit_generator(
                 generator,
-                epochs=self.current_epoch+1,
+                epochs=self.current_epoch + 1,
                 initial_epoch=self.current_epoch,
                 *args, **kwargs
             )
 
             # Update epoch 
             self.current_epoch += 1
-            
+
             # After each epoch predict on test images & show them
             if plot_callback:
                 plot_callback(self.model)
@@ -242,7 +241,7 @@ class PConvUnet(object):
             if self.weight_filepath:
                 print(self.current_weightfile())
                 self.save()
-            
+
     def predict(self, sample):
         """Run prediction using this model"""
         return self.model.predict(sample)
@@ -251,7 +250,7 @@ class PConvUnet(object):
         """Get summary of the UNet model"""
         print(self.model.summary())
 
-    def save(self):        
+    def save(self):
         self.model.save_weights(self.current_weightfile())
 
     def load(self, filepath, train_bn=True, lr=0.0002):
@@ -263,7 +262,7 @@ class PConvUnet(object):
         epoch = int(os.path.basename(filepath).split("_")[0])
         assert epoch > 0, "Could not parse weight file. Should start with 'X_', with X being the epoch"
         self.current_epoch = epoch
-        self.model.load_weights(filepath)        
+        self.model.load_weights(filepath)
 
     def current_weightfile(self):
         assert self.weight_filepath != None, 'Must specify location of logs'
@@ -272,35 +271,35 @@ class PConvUnet(object):
     @staticmethod
     def current_timestamp():
         return datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    
+
     @staticmethod
     def l1(y_true, y_pred):
         """Calculate the L1 loss used in all loss calculations"""
         if K.ndim(y_true) == 4:
-            return K.sum(K.abs(y_pred - y_true), axis=[1,2,3])
+            return K.sum(K.abs(y_pred - y_true), axis=[1, 2, 3])
         elif K.ndim(y_true) == 3:
-            return K.sum(K.abs(y_pred - y_true), axis=[1,2])
+            return K.sum(K.abs(y_pred - y_true), axis=[1, 2])
         else:
             raise NotImplementedError("Calculating L1 loss on 1D tensors? should not occur for this network")
-    
+
     @staticmethod
     def gram_matrix(x, norm_by_channels=False):
         """Calculate gram matrix used in style loss"""
-        
+
         # Assertions on input
         assert K.ndim(x) == 4, 'Input tensor should be a 4d (B, H, W, C) tensor'
-        assert K.image_data_format() == 'channels_last', "Please use channels-last format"        
-        
+        assert K.image_data_format() == 'channels_last', "Please use channels-last format"
+
         # Permute channels and get resulting shape
         x = K.permute_dimensions(x, (0, 3, 1, 2))
         shape = K.shape(x)
         B, C, H, W = shape[0], shape[1], shape[2], shape[3]
-        
+
         # Reshape x and do batch dot product
-        features = K.reshape(x, K.stack([B, C, H*W]))
+        features = K.reshape(x, K.stack([B, C, H * W]))
         gram = K.batch_dot(features, features, axes=2)
-        
+
         # Normalize with channels, height and width
-        gram = gram /  K.cast(C * H * W, x.dtype)
-        
+        gram = gram / K.cast(C * H * W, x.dtype)
+
         return gram
